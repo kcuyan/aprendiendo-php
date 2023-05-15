36c4b1a04ebebb6b1ed082637b46b3aad90a6fad
Kevin Cuyan <kcuyan@outlook.com>
2023-05-15 17:01:19 -0600

    se agrega administracion de productos

--- proyecto-php-poo/controllers/ProductoController.php
+++ proyecto-php-poo/controllers/ProductoController.php
@@ -1,14 +1,59 @@
 <?php
 
-class productoController{
-    public function index(){
+require_once 'models/producto.php';
+
+class productoController {
+
+    public function index() {
         //renderizar vista
         require_once 'views/producto/destacado.php';
     }
-    
+
     public function gestion() {
         Utils::isADmin();
+        $producto = new Producto();
+        $productos = $producto->getAllProductos();
         require_once 'views/producto/gestion.php';
     }
-    
+
+    public function crear() {
+        Utils::isADmin();
+        require_once 'views/producto/crear.php';
+    }
+
+    public function save() {
+        Utils::isADmin();
+        if (isset($_POST)) {
+            $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
+            $descripcion = isset($_POST['descripcion']) ? $_POST['descripcion'] : false;
+            $precio = isset($_POST['precio']) ? $_POST['precio'] : false;
+            $stock = isset($_POST['stock']) ? $_POST['stock'] : false;
+            $categoria = isset($_POST['categoria']) ? $_POST['categoria'] : false;
+            //$imagen = isset($_POST['imagen']) ? $_POST['imagen'] : false;
+            
+            if($nombre && $descripcion && $precio && $stock && $categoria){
+                
+                $producto = new Producto();
+                $producto->setNombre($nombre);
+                $producto->setDescripcion($descripcion);
+                $producto->setPrecio($precio);
+                $producto->setStock($stock);
+                $producto->setCategoria_id($categoria); 
+                $save = $producto->save();
+                
+                if($save){
+                    $_SESSION['producto'] = "Complete";
+                }else{
+                    $_SESSION['producto'] = "failed";
+                }
+            }else{
+                $_SESSION['producto'] = "failed";
+            }
+        }else{
+            $_SESSION['producto'] = "failed";
+        }
+        
+        header('Location:'.base_url.'producto/gestion');
+    }
+
 }
--- proyecto-php-poo/views/producto/gestion.php
+++ proyecto-php-poo/views/producto/gestion.php
@@ -1,1 +1,24 @@
 <h1>Gestión de Productos</h1>
+
+<a href="<?=base_url?>producto/crear" class="button button-small">
+    crear producto
+</a>
+
+<table>
+    <tr>
+        <th>id</th>
+        <th>nombre</th>
+        <th>precio</th>
+        <th>stock</th> 
+    <tr>
+<?php while($prod = $productos->fetch_object()):?>
+    <tr>
+        <td><?= $prod->id; ?></td>
+        <td><?= $prod->nombre; ?></td>
+        <td>Q <?= $prod->precio; ?></td>
+        <td><?= $prod->stock; ?></td>
+    <tr>
+
+<?php endwhile; ?>
+
+</table>
